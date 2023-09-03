e706fe99b0e93ac710ef12aa1cc244c6e4459529
Nataponchobjai <nataponchobjai@gmail.com>
2023-09-03 02:27:38 -0500

    aster

--- main.js
+++ main.js
@@ -1,67 +1,83 @@
-// **THIS IS INCREDIBLY IMPORTANT THAT YOU DO BOTH SECTIONS!!! You will be doing only front-end work in 421 and you need to brush up on your HTML elements**
-
-
-// ***************************
-//          PART ONE
-// ***************************
-// Write a JavaScript program to display the current day and time, start with:
-console.log(new Date)
-
-const displayDate = () => {
-  const currentDate = new Date()
-
-  document.getElementById("display-element").innerHTML = currentDate;
-}
- 
-
-// Write a JavaScript program to convert a number to a string.
-
-
-
-// Write a JavaScript program to convert a string to the number.
-
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
+  let isGreater = 15 > 10;
+  console.log(isGreater)
+  
   // * Null
-  // * Undefined
+  let admin1 = null;
+
+  admin1 = {
+    age : 20,
+    name: "john"
+  };
+
+  admin1 = null
+  console.log(admin1)
+
+  
+  
+  let natapon;  
+
+console.log(natapon);  
+
+
+
+  
   // * Number
+  let us = 35;
+  console.log(us)
+
   // * NaN
+  let result = "text" / 2; 
+  console.log(result); 
+
+
   // * String
+
+let firstName = "Natapon";
+let lastName = 'Chobjai';
+
+
+let fullName = firstName + " " + lastName;
+console.log(fullName); 
   
 
   
 // Write a JavaScript program that adds 2 numbers together.
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+
+console.log(sum);
 
 
 
 // Write a JavaScript program that runs only when 2 things are true.
 
+let massNatapon = 250;
+let massPhaksaya = 140;
+console.log(massNatapon > massPhaksaya)
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
-
+let num3 =20;
+let num4 =30;
+if (num3 < num4) {
+  console.log("1 of 2 thing are true")
+} 
 
 
 // Write a JavaScript program that runs when both things are not true.  
+let woman1 = false;
+let woman2 = false;
 
-// ***************************
-//         PART TWO
-// ***************************
 
-// 1. download Live-Server by Ritwick Dey, 
-// 2. reload VS Code, 
-// 3. click the "Go Live" button
-// 4. Go local host 5500 or http://127.0.0.1:5500/index.html to see your web page
-// 5. Or go use the `npm start` command and navigate to localhost:8080 (ctrl + C to close)
-// 6. go to `index.html` 
-// 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
+if (!woman1 && !woman2) {
+  console.log("Both things are not true.");
+}
 
 
 
 
-// Additional Resources
-// Video1: https://player.vimeo.com/video/377147232
-// Video2: https://www.youtube.com/embed/bkvH28PXLWc
-// Video3: https://www.youtube.com/embed/TrGI9Yki-24
